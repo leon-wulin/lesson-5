e09eddba00c46bc3b85b9298be63fac6ceb5ecb6
leon-wulin <55013445+leon-wulin@users.noreply.github.com>
2019-10-26 13:53:08 +0800

    updated

---
+++ the ending of math.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+
+num1 = input('Type a number:')
+num2 = input('Type a number:')
+num3 = input('Type a number:')
+num4 = input('Type a number:')
+num5 = input('Type a number:')
+
+mean_mid=0
+mean_fin=0
+
+mean_mid=num1+num2+num3+num4+num5
+mean_fin=mean_mid/5
+print(mean_fin)
